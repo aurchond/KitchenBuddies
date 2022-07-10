fdd4ba7eaf395d980af2dca3901931738e5c1b0e
Maya Bishop <maya.bishop@uwaterloo.ca>
2022-07-10 11:18:55 -0400

    creating input processing for recipe

---
+++ Server/src/main/java/org/input_processing/Main.java
@@ -0,0 +1,8 @@
+package org.input_processing;
+
+public class Main {
+    // Will handle parsing steps apart before the recipe Creator gets called
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
