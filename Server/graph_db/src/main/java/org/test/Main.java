00f10042ab0a7c4143e3cd2a9bbfc007926a1f2f
Caleb Chacko <ctchacko@uwaterloo.ca>
2022-07-11 13:09:01 -0400

    Created sample recipe entry and fixed database session issue

--- Server/graph_db/src/main/java/org/test/Main.java
+++ Server/graph_db/src/main/java/org/test/Main.java
@@ -1,6 +1,8 @@
 package org.test;
 
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 import org.neo4j.graphdb.Path;
 import org.neo4j.ogm.model.Result;
@@ -12,58 +14,103 @@ import org.neo4j.ogm.config.Configuration;
 public class Main {
     public static void main(String[] args) {
 
+        buildSampleRecipe();
+
+//        System.out.println(session.countEntitiesOfType(Step.class) + " stations saved");
+//        getRoute("Step 1", "Step 3", session);
+//        System.out.println(session.load(Step.class, s3.getRecipeID()).getStepID());
+
+    }
+
+    private static void buildSampleRecipe() {
         System.out.println("Hello world!");
         String uri = "neo4j+s://db42e3f1.databases.neo4j.io";
         Configuration configuration = new Configuration.Builder()
                 .uri(uri)
                 .credentials("neo4j", "9hCaQ7nmAyf5AAkUwjrk5lY8ejC61PYa2-4-zLBc6hg")
                 .build();
-        //"com.javahelps.ogmhelloworld.entities"
         SessionFactory sessionFactory = new SessionFactory(configuration, "org.test");
         final Session session = sessionFactory.openSession();
         Transaction tx = session.beginTransaction();
 
-        Step s1 = new Step("Step 1");
-        Step s2 = new Step("Step 2");
-        Step s3 = new Step("Step 3");
-
-        s1.addConnection(s2, 52);
-        s1.addConnection(s3, 49);
-        s2.addConnection(s3, 20);
-        s2.addConnection(s1, 95);
-
-        session.save(s1);
-        session.save(s2);
-        session.save(s3);
-        tx.commit();
-
-        System.out.println(session.countEntitiesOfType(Step.class) + " stations saved");
-//        getRoute("Step 1", "Step 3", session);
-
-        System.out.println(session.load(Step.class, s1.getId()).getName());
-
+        tx = createAsparagus(tx, session);
         tx.close();
     }
 
-    private static void getRoute(final String from, final String destination, final Session session) {
-        System.out.printf("searching for the shortest route from %s to %s..\n", from, destination);
+    //https://www.simplyrecipes.com/recipes/asparagus/
+    private static Transaction createAsparagus(Transaction tx, Session s) {
+        // Step 1
+        // Boil water in saucepan
+        List<String> r1_ingredients = new ArrayList<String>();
+        r1_ingredients.add("salted water");
 
-        String cypherQuery = String.format(
-                "MATCH (from:TrainStation {name:'%s'}), (to:TrainStation {name:'%s'}), paths=allShortestPaths((from)-[:LEADS_TO*]->(to)) " +
-                        "WITH REDUCE(dist = 0, rel in rels(paths) | dist + rel.distance) AS distance, paths " +
-                        "RETURN paths, distance " +
-                        "ORDER BY distance " +
-                        "LIMIT 1",
-                from, destination);
-        final Result result = session.query(cypherQuery, Collections.emptyMap());
-        System.out.printf("shortest way from %s to %s via\n", from, destination);
-        result.queryResults().forEach(entry -> {
-            Long distance = (Long) entry.get("distance");
-            Path path = (Path) entry.get("paths");
-            System.out.printf("distance: %s\n", distance);
-            path.nodes().forEach(node -> {
-                System.out.printf("- %s\n", node.getProperty("name"));
-            });
-        });
+        List<Number> r1_quantity = new ArrayList<Number>();
+        r1_quantity.add(0.5);
+
+        List<String> r1_tools = new ArrayList<String>();
+        Step s1 = new Step(25L, 1, true, "saucepan",
+                0, 1, 10, r1_ingredients, r1_quantity, r1_tools);
+
+        // Step 2
+        // Prepare asparagus
+        // Prepare the asparagus while the water is heating. Rinse them thoroughly. Break off any tough, white bottoms and discard.
+        //
+        //Cut the spears into 1- to 2-inch sections, slicing the asparagus at a slight diagonal.
+        List<String> r2_ingredients = new ArrayList<String>();
+        r2_ingredients.add("asparagus");
+        List<Number> r2_quantity = new ArrayList<Number>();
+        r2_quantity.add(1);
+
+        List<String> r2_tools = new ArrayList<String>();
+        r2_tools.add("knife");
+
+        Step s2 = new Step(25L, 2, true, "cutting board",
+                0, 5, 5, r2_ingredients, r2_quantity, r2_tools);
+
+        // Step 3
+        // Boil asparagus and lower heat
+        List<String> r3_ingredients = new ArrayList<String>();
+        r2_ingredients.add("asparagus");
+        List<Number> r3_quantity = new ArrayList<Number>();
+        r2_quantity.add(1);
+
+        Step s3 = new Step(25L, 3, false, "saucepan",
+                0, 10, 20, r2_ingredients, r2_quantity, r2_tools);
+
+        // Step 4
+        // Drain water, add ingredients
+        // Toss the asparagus with olive oil, parmesan, and lemon zest while it's still hot.
+        //
+        //Salt and pepper to taste. Serve warm or room temperature.
+        List<String> r4_ingredients = new ArrayList<String>();
+        r2_ingredients.add("asparagus");
+        r2_ingredients.add("olive oil");
+        r2_ingredients.add("parmesan");
+        r2_ingredients.add("lemon zest");
+        List<Number> r4_quantity = new ArrayList<Number>();
+        r2_quantity.add(1);
+        r2_quantity.add(2);
+        r2_quantity.add(2);
+        r2_quantity.add(1);
+
+        List<String> r4_tools = new ArrayList<String>();
+        r2_tools.add("spoon");
+
+        Step s4 = new Step(25L, 4, false, "bowl",
+                0, 5, 25, r4_ingredients, r4_quantity, r4_tools);
+
+        // Step 1 -> Step 3 with 10 timeleft
+        s1.addConnection(s3, 10);
+        // Step 2 -> Step 3 with 5 timeleft
+        s2.addConnection(s3, 5);
+        // Step 3 -> Step 4 with 20 timeleft
+        s3.addConnection(s4, 20);
+
+        s.save(s1);
+        s.save(s2);
+        s.save(s3);
+        tx.commit();
+
+        return tx;
     }
 }
--- Server/graph_db/src/main/java/org/test/Step.java
+++ Server/graph_db/src/main/java/org/test/Step.java
@@ -1,35 +1,95 @@
 package org.test;
 
+import java.util.List;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
+import org.neo4j.ogm.annotation.GeneratedValue;
+import org.neo4j.ogm.annotation.Id;
 import org.neo4j.ogm.annotation.NodeEntity;
 import org.neo4j.ogm.annotation.Relationship;
 
-@NodeEntity(label="Step")
+@NodeEntity(label="Test_Step")
+//this defines our step node and all its attributes
 public class Step {
-    private Long id;
-
     private String name;
+
+     //TODO: make ID like recipeID.stepID double?
+    private Long recipeID; //number identifying recipe in database
+    @Id
+    private Integer stepID; //step number from original recipe
+    private Boolean prepStep; //defines if it is a prep step
+    private String holdingResource; //what the ingredients are being kept in
+    private Integer holdingID; //0 = for vegetables, 1 = for meat, 2 = mixed
+    private Integer stepTime; //how much time it takes to complete step
+    private Integer timeLeft; //how much time remaining to complete original recipe
+    private List<String> ingredientList; //names of ingredients
+    private List<Number> ingredientQuantity; //quantities of ingredients
+    private List<String> resourcesRequired; //kitchen equipment like knife, cheese grater, etc.
+
 
     @Relationship(type = Connection.TYPE, direction = Relationship.OUTGOING)
     private Set<Connection> connections = new HashSet<>();
 
-    public Step(String name) {
-        this.name = name;
+    //most used constructor
+    public Step(Long recipeID, Integer stepID, Boolean prepStep, String holdingResource,
+                Integer holdingID, Integer stepTime, Integer timeLeft, List<String> ingredientList,
+                List<Number> ingredientQuantity, List<String> resourcesRequired) {
+        this.recipeID = recipeID;
+        this.stepID = stepID;
+        this.prepStep = prepStep;
+        this.holdingResource = holdingResource;
+        this.holdingID = holdingID;
+        this.stepTime = stepTime;
+        this.timeLeft = timeLeft;
+        this.ingredientList = ingredientList;
+        this.ingredientQuantity = ingredientQuantity;
+        this.resourcesRequired = resourcesRequired;
+        this.name = stepID.toString();
+    }
+
+    //for steps like: "preheat oven to 350 degrees", there are no ingredients or resources required
+    public Step(Long recipeID, Integer stepID, Boolean prepStep, String holdingResource,
+                Integer holdingID, Integer stepTime, Integer timeLeft) {
+        this.recipeID = recipeID;
+        this.stepID = stepID;
+        this.prepStep = prepStep;
+        this.holdingResource = holdingResource;
+        this.holdingID = holdingID;
+        this.stepTime = stepTime;
+        this.timeLeft = timeLeft;
     }
 
     public void addConnection(Step target, int distance) {
         this.connections.add(new Connection(this, target, distance));
     }
 
-    public String getName() {
-        return this.name;
+    //getter functions
+    public Long getRecipeID() { return this.recipeID; }
+    public Integer getStepID() {
+        return this.stepID;
     }
+    public Boolean checkIfPrep() { return this.prepStep; }
+    public String getHoldingResource() { return this.holdingResource; }
+    public Integer getHoldingID() { return this.holdingID; }
+    public Integer getStepTime() { return this.stepTime; }
+    public Integer getTimeLeft() { return this.timeLeft; }
+    public List<String> getIngredientList() { return this.ingredientList; }
+    public List<Number> getIngredientQuantity() { return this.ingredientQuantity; }
+    public List<String> getResourcesRequired() { return resourcesRequired; }
 
-    public Long getId() {
-        return this.id;
-    }
+    //setter functions
+    public void setRecipeID(Long ID) { this.recipeID = ID; }
+    public void setStepID(Integer ID) { this.stepID = ID; }
+    public void setPrepStep(Boolean prep) { this.prepStep = prep; }
+    public void setHoldingResource(String holdingResource) { this.holdingResource = holdingResource; }
+    public void setHoldingID(Integer holdingID) { this.holdingID = holdingID; }
+    public void setStepTime(Integer stepTime) { this.stepTime = stepTime; }
+    public void setTimeLeft(Integer timeLeft) { this.timeLeft = timeLeft; }
+    public void setIngredientList(List<String> ingredientList) { this.ingredientList = ingredientList; }
+    public void setIngredientQuantity(List<Number> ingredientQuantity) { this.ingredientQuantity = ingredientQuantity; }
+    public void setResourcesRequired(List<String> resourcesRequired) { this.resourcesRequired = resourcesRequired; }
 
-    // hashCode, equals, toString, getter/setter, no-arg constructor ommitted..
+// hashCode, equals, toString, no-arg constructor ommitted..
 }
