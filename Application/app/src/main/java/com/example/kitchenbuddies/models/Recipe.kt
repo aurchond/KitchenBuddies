9568b3218ad29b2eaedcd9925e8dd59333f370d1
Aurchon Datta <aurchond@gmail.com>
2022-07-18 17:09:18 -0400

    remove old application side and added new client side folder

--- Application/app/src/main/java/com/example/kitchenbuddies/models/Recipe.kt
+++
@@ -1,9 +0,0 @@
-package com.example.kitchenbuddies.models
-
-class Recipe (
-    var name: String,
-    var cook_time: String,
-    var difficulty: String,
-    var previous_meal_session: String,
-    var expandable: Boolean = true
-)
--- Application/app/src/main/java/com/example/kitchenbuddies/ui/helper/Recipe.kt
+++
@@ -1,8 +0,0 @@
-package layout
-
-class Recipe (
-    val name: String,
-    val cook_time: String,
-    val difficulty: String,
-    val previous_meal_session: String,
-    var expandable: Boolean = true)
