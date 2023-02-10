6b72a80705955e393d9d73f6cafe6397863b41e0
Caleb Chacko <ctchacko@uwaterloo.ca>
2023-02-10 15:12:14 -0500

    Added check_ingredient_db psuedocode to mysql_db.py

---
+++ Server/Py_Text_Processing/mysql_db.py
@@ -0,0 +1,19 @@
+import mysql.connector
+
+def verify_ingredients_supplies(istr_nouns):
+    '''
+    Check which nouns within the sentence are ingredients
+
+    :istr_nouns: List of nouns within recipe instruction
+    :return: List of ingredients
+    '''
+
+    # create ingredient query string
+    # create supply query string
+
+    # connect to database
+
+    # run ingredient query - return only ingredients
+    # run supply query - return only supplies
+
+    # return (ingredients, supplies)
