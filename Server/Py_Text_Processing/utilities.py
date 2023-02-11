0a949e743471e3f8ec19729869dfe1777f83cfad
Caleb Chacko <ctchacko@uwaterloo.ca>
2023-02-11 16:32:46 -0500

    Updated filepath for utility function

--- Server/Py_Text_Processing/utilities.py
+++ Server/Py_Text_Processing/utilities.py
@@ -3,7 +3,7 @@ def extract_recipe_text(filename):
     instr_steps = []
     # Parse recipe text into array
     count = 0
-    with open ("./input/"+filename, 'rt') as recipe:
+    with open ("./Py_Text_Processing/input/"+filename, 'rt') as recipe:
         for instr_line in recipe:
             mini_steps = instr_line.split('.')
             # print(mini_steps)
