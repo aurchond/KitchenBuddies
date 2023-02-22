776146d12b6a17135e7f7a9fd5e11fac48ff7b2f
sszargar <sszargar@uwaterloo.ca>
2023-02-22 13:14:39 -0500

    update import paths

--- first_app/lib/screens/all_recipes_screen.dart
+++ first_app/lib/screens/all_recipes_screen.dart
@@ -1,4 +1,4 @@
-import 'package:first_app/widgets/input_text_button.dart';
+import 'package:first_app/helpers/input_text_button.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
