61a4cb68e88914bcdbdfc2810399e233ba48cc24
sszargar <sszargar@github.com>
2023-03-02 22:57:17 +0000

    Commented out tests

--- Server/src/test/java/org/utilities/database/graph/test_graph.java
+++ Server/src/test/java/org/utilities/database/graph/test_graph.java
@@ -6,17 +6,17 @@ import org.recipe_processing.Recipe;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class test_graph {
-    @Test
-    public void testIsRecipeInDatabase() {
-        assertEquals(RecipeHelper.isRecipeInDatabase("rotini"), Boolean.TRUE);
-    }
+    // @Test
+    // public void testIsRecipeInDatabase() {
+    //     assertEquals(RecipeHelper.isRecipeInDatabase("rotini"), Boolean.TRUE);
+    // }
 
-    @Test
-    public void testGetRecipeFromDatabase() {
-        Recipe recipe = RecipeHelper.getRecipeFromDatabase("rotini");
-        System.out.println(recipe.getFinalStep().getIngredientList().get(0));
-        assertEquals(recipe.getFinalStep().getIngredientList().get(0), "casserole");
-    }
+    // @Test
+    // public void testGetRecipeFromDatabase() {
+    //     Recipe recipe = RecipeHelper.getRecipeFromDatabase("rotini");
+    //     System.out.println(recipe.getFinalStep().getIngredientList().get(0));
+    //     assertEquals(recipe.getFinalStep().getIngredientList().get(0), "casserole");
+    // }
 
 
 }
