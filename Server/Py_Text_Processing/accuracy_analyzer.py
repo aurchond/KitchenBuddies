800861a6764892d86f0eb56fb8e4873ff02889c9
sszargar <sszargar@github.com>
2023-03-03 00:54:10 +0000

    Adding premium processing

---
+++ Server/Py_Text_Processing/accuracy_analyzer.py
@@ -0,0 +1,9 @@
+# read expected and generated outputs
+
+# create dictionary of marks per item
+
+# iterate through each section of each step and check how many items are correct
+
+# print out accuracy
+
+# this is premium processing
